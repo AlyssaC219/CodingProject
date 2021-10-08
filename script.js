8ef061d8ffa1c0946022554e083a429912458305
AlyssaC219 <tcc353@nyu.edu>
2021-10-07 21:10:34 -0400

    Draft

---
+++ script.js
@@ -0,0 +1,17 @@
+let videos = [
+    {
+        thumbnail: "IMG_3244.HEIC",
+        videoTitle: "Sleepy GOlden Retriever", 
+        Author: "Alyssa",
+    }, 
+    {
+        thumbnail:"IMG_3657.HEIC",
+        videoTitle: "Corgi Cola",
+        Author: "Alyssa",
+    }, 
+    {
+        thumbnail:"IMG_3802.HEIC", 
+        videoTitle: ""
+    }
+]
+
